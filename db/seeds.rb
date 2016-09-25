3040e43c4186f2a59dacdd41447103f5bfaf0762
Anna Wu <shinshinwu@gmail.com>
2016-09-25 09:25:21 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,58 @@
 
 
 host_1 = Host.new(
-                    :first_name =>"Woddy",
+                    :first_name =>"Woody",
                     :last_name => "Allen",
-                    :email => "host@gmail.com",
+                    :email => "host1@gmail.com",
+                    :phone => "1231231234"
                     :password => "password123")
 host_1.save
-puts host_1
+
+host_2 = Host.new(
+                    :first_name =>"Christopher",
+                    :last_name => "Nolan",
+                    :email => "host2@gmail.com",
+                    :password => "password123")
+host_2.save
+
+host_3 = Host.new(
+                    :first_name =>"Sophia",
+                    :last_name => "Copla",
+                    :email => "host3@gmail.com",
+                    :password => "password123")
+host_3.save
 
 
 participant_1 = Participant.new(
                     :first_name =>"Sancho",
 										:last_name => "Pansa",
-										:email => "participant@gmail.com",
+										:email => "participant1@gmail.com",
+                    monthly_household_income: 2100,
+                    household_member_size: 3,
+                    voucher_number: "323NLASD",
 										:password => "password123")
 participant_1.save
-puts participant_1
+
+
+participant_2 = Participant.new(
+                    :first_name =>"Avril",
+                    :last_name => "Bethers",
+                    monthly_household_income: 2500,
+                    household_member_size: 4,
+                    voucher_number: "ADR7889",
+                    :email => "participant2@gmail.com",
+                    :password => "password123")
+participant_2.save
+
+participant_3 = Participant.new(
+                    :first_name =>"Blake",
+                    :last_name => "Winston",
+                    monthly_household_income: 3000,
+                    household_member_size: 6,
+                    voucher_number: "6YHC246",
+                    :email => "participant3@gmail.com",
+                    :password => "password123")
+participant_3.save
 
 
 admin_1 = Admin.new(:first_name =>"Boss",
@@ -51,15 +88,120 @@ dwelling_1 = Dwelling.create(
 dwelling_1.programs << Program.first(2)
 
 Address.create(
-  addressable_id: 1,
+  addressable_id: dwelling_1.id,
   addressable_type: "Dwelling",
   phone: "1231231234",
   line_1: "201 S Market St",
   city: "San Jose", state: "CA",
-  zipcode: "95113",
-  lat: 37.331436,
-  lng: -121.890214
+  zipcode: "95113"
 )
+
+dwelling_2 = Dwelling.create(
+  host_id:        host_3.id,
+  display_name:   "San Jose cozy 1 bedroom",
+  monthly_rent:   2500,
+  num_rooms:      1,
+  num_bathrooms:  1,
+  property_type:  "apartment",
+  is_available:   true
+)
+
+dwelling_2.programs << Program.last(2)
+
+Address.create(
+  addressable_id: dwelling_2.id,
+  addressable_type: "Dwelling",
+  phone: "1231231234",
+  line_1: "1628 Hostetter Rd",
+  city: "San Jose", state: "CA",
+  zipcode: "95131"
+)
+
+dwelling_3 = Dwelling.create(
+  host_id:        host_2.id,
+  display_name:   "Perfect size for a family in North Valley",
+  monthly_rent:   3880,
+  num_rooms:      3,
+  num_bathrooms:  1,
+  property_type:  "house",
+  is_available:   true
+)
+
+Address.create(
+  addressable_id: dwelling_3.id,
+  addressable_type: "Dwelling",
+  phone: "1231231234",
+  line_1: "1696 Berryessa Rd",
+  city: "San Jose", state: "CA",
+  zipcode: "95133"
+)
+
+dwelling_3.programs << Program.last
+
+dwelling_4 = Dwelling.create(
+  host_id:        host_2.id,
+  display_name:   "1 bedroom with parking near Alamo Square",
+  monthly_rent:   2150,
+  num_rooms:      1,
+  num_bathrooms:  1,
+  property_type:  "apartment",
+  is_available:   true
+)
+
+Address.create(
+  addressable_id: dwelling_4.id,
+  addressable_type: "Dwelling",
+  phone: "1231231234",
+  line_1: "1350 Golden Gate Ave",
+  line_2: "#10",
+  city: "San Francisco", state: "CA",
+  zipcode: "94115"
+)
+
+dwelling_4.programs << Program.last(2)
+
+dwelling_5 = Dwelling.create(
+  host_id:        host_3.id,
+  display_name:   "Spacious Studio in Mission",
+  monthly_rent:   3880,
+  num_rooms:      0,
+  num_bathrooms:  1,
+  property_type:  "house",
+  is_available:   true
+)
+
+Address.create(
+  addressable_id: dwelling_5.id,
+  addressable_type: "Dwelling",
+  phone: "1231231234",
+  line_1: "375 Alabama St",
+  line_2: "#100",
+  city: "San Francisco", state: "CA",
+  zipcode: "94110"
+)
+
+dwelling_5.programs << Program.first(2)
+
+dwelling_6 = Dwelling.create(
+  host_id:        host_1.id,
+  display_name:   "Russian Hill gem",
+  monthly_rent:   3660,
+  num_rooms:      2,
+  num_bathrooms:  2,
+  property_type:  "apartment",
+  is_available:   true
+)
+
+Address.create(
+  addressable_id: dwelling_6.id,
+  addressable_type: "Dwelling",
+  phone: "1231231234",
+  line_1: "1958 Polk St",
+  city: "San Francisco", state: "CA",
+  zipcode: "94109"
+)
+
+dwelling_6.programs << Program.first
 
 DwellingApplication.create(
   dwelling_id:  dwelling_1.id,
