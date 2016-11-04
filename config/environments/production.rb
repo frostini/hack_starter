bc719138da6946bbd73fa6a26c1090895c3e9dd3
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-11-04 10:10:07 -0700

    configured in production as welll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'public-bnb-app.herokuapp.com/' }
+  # CONFIGURING MAILER FOR SPARPOST TRANSACTIONAL
+  config.action_mailer.delivery_method = :sparkpost
+  # config.action_mailer.default_url_options = { host: 'public-bnb-app.herokuapp.com/' } 
 end
